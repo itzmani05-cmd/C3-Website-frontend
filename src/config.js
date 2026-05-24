9ac7b40977b7331ef1a51a6b116b71ba850bc521
itzmani-cmd <itzmani-cmd>
2026-05-24 17:00:59 +0530

    fixed the url issue

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const BACKEND_URL = "https://c3website-backend.onrender.com";
+// export const BACKEND_URL = "https://c3website-backend.onrender.com";
+export const BACKEND_URL = "https://c3websitebackend.onrender.com";
